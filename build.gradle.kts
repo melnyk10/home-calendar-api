abdaa0d99621cc0f44ac9c3ffe8f0dd87b2ca148
omelnyk10 <orest.melnyk@sigma.software>
2025-07-23 21:21:48 +0300

    Add quarkus-config-yaml for supporting application.yaml config

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-rest")
+    implementation("io.quarkus:quarkus-config-yaml")
+
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
 }
