c5686dadaff435ca37517168b028563b9f8c38cb
omelnyk10 <orest.melnyk@sigma.software>
2025-09-04 23:47:44 +0300

    add todo

--- src/main/java/com/meln/app/calendar/provider/google/GoogleCalendarClient.java
+++ src/main/java/com/meln/app/calendar/provider/google/GoogleCalendarClient.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @ApplicationScoped
 @RequiredArgsConstructor
+//todo: Make the client stateless
 public class GoogleCalendarClient implements CalendarClient<GoogleCalendarProperties> {
 
   @Override
